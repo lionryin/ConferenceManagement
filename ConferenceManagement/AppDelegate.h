c34f78cbe00bf115301a25339eb39ecff2acae75
lionryin <710523800@qq.com>
2015-04-11 21:30:11 +0800

    Initial Commit

---
+++ ConferenceManagement/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ConferenceManagement
+//
+//  Created by MacAir2 on 15/4/11.
+//  Copyright (c) 2015年 lixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ConferenceManagement/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ConferenceManagement
+//
+//  Created by MacAir2 on 15/4/11.
+//  Copyright (c) 2015年 lixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
